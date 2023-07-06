1b04e71f42d0e68caf08f0510a21d3b550482602
Richard Moss <richard@englishtutorslive.com>
2023-07-06 16:33:04 +0100

    Setup basic api call in nextJS serverless function

---
+++ openai-code-practices-app/pages/api/completion.ts
@@ -0,0 +1,13 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  organization: process.env.OPENAIA_ORG,
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  const response = await openai.listEngines()
+  res.status(200).json({ data: response.data.data })
+}
